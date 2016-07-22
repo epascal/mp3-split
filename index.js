6468c19c4c7de709bc0bde35a42004b74bdb63ea
Eric PASCAL <eric.pascal@aim-services.ch>
2016-07-22 06:16:58 +0200

    correction pb de chemin de l'exe pour installation sur windows

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const execSync = require('child_process').execSync;
 
 function splitFile(fileName, index) {
-    execSync('mp3splt\\mp3splt.exe -f -T 12 -t 5.0 -o ' + ('00'+index).slice(-2) + '@h@m@s-@f "' + process.argv[2]+"\\"+fileName + '"');
+    execSync(process.argv[1].substr(0, process.argv[1].lastIndexOf('\\'))+'\\mp3splt\\mp3splt.exe -f -T 12 -t 5.0 -o ' + ('00'+index).slice(-2) + '@h@m@s-@f "' + process.argv[2]+"\\"+fileName + '"');
     fs.unlinkSync(process.argv[2]+"\\"+fileName);
 }
 
